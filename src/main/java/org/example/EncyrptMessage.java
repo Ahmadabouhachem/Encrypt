879bc1cc6b6aaae91de7e7b8b3be5e34c5b9c6a5
Ahmadabouhachem <ahmedaboha@hotmail.com>
2024-05-19 20:58:55 +0200

    encrypt a message with key value

---
+++ src/main/java/org/example/EncyrptMessage.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class EncyrptMessage {
+    public static void main(String[] args) {
+        System.out.println(encryptedMessage("defghij", 9));
+    }
+
+    public static String encryptedMessage(String numberStr, int k) {
+        char[] alphabets = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        char[] alphabetEncrypt = numberStr.toUpperCase().toCharArray();
+        String encryptedWord = "";
+        for (int i = 0; i < alphabetEncrypt.length; i++) {
+
+            for (int j = 0; j < alphabets.length; j++) {
+                if (alphabetEncrypt[i] == alphabets[j]) {
+                    if (j - k < 0) {
+                        encryptedWord += alphabets[alphabets.length + j - k];
+                    } else {
+                        encryptedWord += alphabets[j - k];
+                    }
+                }
+            }
+
+        }
+        return encryptedWord;
+    }
+}
